bce4ae5eae8314abd02608b3e8ccbde709e03720
Abdul Wajid Afridi <wajidafridi8554@gmail.com>
2023-10-29 23:19:36 -0700

    first commit

--- App.tsx
+++ App.tsx
@@ -1,32 +1,15 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-import ReAnimatedLayout from "./components/LayoutAnimation/ReAnimatedLayout";
-import TodosApp from "./components/LayoutAnimation/Todos";
-import AnimationApp from "./components/AnimationApp";
-import SpinnerLoading from "./components/animations/spinner";
-import Spinner from "./components/UpdatedSpinner";
-// import Spinner from "./components/spinner/Spinner";
+import Branch2 from "./components/branch-2";
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      {/* <AnimationApp/> */}
-      {/* <ReAnimatedLayout/> */}
-      {/* <TodosApp/> */}
-      {/* <SpinnerLoading/> */}
-      {/* <Spinner/> */}
-      {/* <Spinner /> */}
-      <Spinner size={40} dots={3} />
+    <View>
+      <Branch2 />
     </View>
   );
 };
 
 export default App;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    display: "flex",
-    justifyContent: "center",
-  },
-});
+const styles = StyleSheet.create({});
---
+++ components/branch-2.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Branch2 = () => {
+  return (
+    <View>
+      <Text>Branch2 "first commit"</Text>
+      <Text>Branch2 "2nd commit"</Text>
+      <Text>Branch2 "3rd commit"</Text>
+    </View>
+  );
+};
+
+export default Branch2;
+
+const styles = StyleSheet.create({});
